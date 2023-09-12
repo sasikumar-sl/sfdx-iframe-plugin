0f1cd7d36e27384a52f4ef69439cf7a6b594985a
sasikumar-sl <sasikumar.ganesan@supportlogic.io>
2023-09-12 14:35:57 +0530

    Initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+
+  ul {
+    height: 100%;
+    min-height: 400px;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin-right: 10px;
+    width: 200px;
+    font-size: 16px;
+  }
+
+  button {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }
+  
+`;
