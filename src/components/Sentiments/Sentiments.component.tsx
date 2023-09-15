0f51f80ec4fa78df46be17d795d7fc0830309e79
sasikumar-sl <sasikumar.ganesan@supportlogic.io>
2023-09-15 23:18:06 +0530

    Tooltip check

--- src/components/Sentiments/Sentiments.component.tsx
+++ src/components/Sentiments/Sentiments.component.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import { SentimentsContainer, ScoreWrapper, ScoreCardsWrapper } from './Sentiments.styles';
+import {
+    SentimentsContainer,
+    Scorers,
+    ScoreWrapper,
+    ScoreCardsWrapper,
+    StyledInfoIcon,
+    StyledScore
+} from './Sentiments.styles';
 
 export const Sentiments: React.FC = () =>  {
 return (
     <SentimentsContainer>
-        <ScoreWrapper/>
+        <Scorers>
+            <ScoreWrapper>
+                <StyledScore  type="Sentiment" value={35} showTooltip/>
+                <StyledInfoIcon/>
+            </ScoreWrapper>
+            <ScoreWrapper>
+                <StyledScore  type="Attention" label="Attention Score" value={50} />
+                <StyledInfoIcon/>
+            </ScoreWrapper>
+        </Scorers>
         <ScoreCardsWrapper/>
     </SentimentsContainer>
     );
--- src/components/Sentiments/Sentiments.styles.ts
+++ src/components/Sentiments/Sentiments.styles.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { ReactComponent as InfoIcon } from '../../icons/circled-information.svg'; 
+import { Score } from '@supportlogic/frontend-library';
 
 
 export const SentimentsContainer = styled.div`
@@ -7,14 +9,40 @@ export const SentimentsContainer = styled.div`
 `;
 
 
-export const ScoreWrapper = styled.div`
+export const Scorers = styled.div`
     height: 62px;
+    width: 100%;
     background: #fff;
+    display: inline-flex;
+    justify-content: center;
+    gap: 40px;
+`;
+
+export const ScoreWrapper = styled.div`
+    display: inline-flex;
+    padding: 4px;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
 `;
 
 export const ScoreCardsWrapper = styled.div`
     height: 190px;
     flex-shrink: 0;
     background: #F8F8F8;
+`;
+
+export const StyledScore = styled(Score)`
+    gap: 8px !important;
+    color: #4A4A4A;
+    font-family: Avenir;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    div:nth-child(2) {
+    }
+`;
+export const StyledInfoIcon = styled(InfoIcon)`
 `;
 
