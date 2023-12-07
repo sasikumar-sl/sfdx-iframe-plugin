de54673aea7b267fcd7602c5f8cf9812f79323ca
vasantha.kumar <vasanthakumar.sankaran_c@supportlogic.io>
2023-12-07 15:26:40 +0000

    NPM_TOKEN

--- docker-build.sh
+++ docker-build.sh
@@ -15,7 +15,7 @@ IMAGE_NAME=$BASE_NAME:$TAG
 # Note: doing this because under GitHub Actions environment, there
 # doesn't seem to exist a .git folder we can copy into the container
 # in order for the webpack git hash module to grab the hash from.
-$BUILD_TOOL build . -t $IMAGE_NAME --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg NPM_TOKEN="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"
+$BUILD_TOOL build . -t $IMAGE_NAME --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg NPM_TOKEN="$NPM_TOKEN"
 BUILD_RESULT=$?
 if [ $BUILD_RESULT -ne 0 ]; then
     echo
